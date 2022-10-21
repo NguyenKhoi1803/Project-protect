71ab1b68e1b0e6f4a1e4c00e5f7bafcb94beef8b
Nguyen Khoi <nguyenkhoi180397@gmail.com>
2022-10-22 00:16:32 +0700

    update-payments

--- src/components/User/Payments/index.js
+++ src/components/User/Payments/index.js
@@ -1,13 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
 function Payments() {
   const paymentsArr = useSelector((state) => state.fetchTourReducer.cart);
-
   console.log("paymentsArr", paymentsArr);
-  return <div>
-    {paymentsArr.nameTour}
-  </div>;
+
+  const [formFields, setFormFields] = useState([
+    {
+      name: "",
+      phone: "",
+      address: "",
+      sex: "",
+      idCard: "",
+      age: "",
+    },
+  ]);
+
+  const handleFormChange = (event, index) => {
+    let data = [...formFields];
+    data[index][event.target.name] = event.target.value;
+    setFormFields(data);
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+  };
+
+  const addFields = () => {
+    let object = {
+      name: "",
+      phone: "",
+      address: "",
+      sex: "",
+      idCard: "",
+      age: "",
+    };
+    setFormFields([...formFields, object]);
+  };
+
+  const removeFields = (index) => {
+    let data = [...formFields];
+    data.splice(index, 1);
+    setFormFields(data);
+  };
+
+  return (
+    <div className="payments__form">
+      <form onSubmit={submit}>
+        {formFields.map((form, index) => {
+          return (
+            <div key={index}>
+              <input
+                name="name"
+                placeholder="Name"
+                onChange={(event) => handleFormChange(event, index)}
+                value={form.name}
+              />
+              <input
+                name="phone"
+                placeholder="Phone"
+                onChange={(event) => handleFormChange(event, index)}
+                value={form.phone}
+              />
+              <input
+                name="address"
+                placeholder="Address"
+                onChange={(event) => handleFormChange(event, index)}
+                value={form.address}
+              />
+              <input
+                name="idCard"
+                placeholder="ID Card"
+                onChange={(event) => handleFormChange(event, index)}
+                value={form.idCard}
+              />
+              <input
+                name="age"
+                placeholder="Age"
+                onChange={(event) => handleFormChange(event, index)}
+                value={form.age}
+              />
+              <button onClick={() => removeFields(index)}> Remove</button>
+            </div>
+          );
+        })}
+      </form>
+      <button onClick={addFields}> Thêm Người </button>
+      <button onClick={submit}> Hoàn Thành </button>
+    </div>
+  );
 }
 
 export default Payments;
