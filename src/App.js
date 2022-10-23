8a517646fb6ec5870df63710afb8d9c381cb2ed5
Nguyen Khoi <nguyenkhoi180397@gmail.com>
2022-10-24 00:50:22 +0700

    update

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import SearchList from "./components/User/SearchForm/searchList";
 import Payments from "./components/User/Payments";
 import Login from "./components/User/Login";
 import Register from "./components/User/Register";
+import SucceedPage from "./layout/SucceedPage";
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
           <Route path="/products/cart/:id" element={<Payments />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/register" element={<Register />}></Route>
+          <Route path="/succeed" element={<SucceedPage />}></Route>
         </Routes>
         <Footer />
       </ThemeProvider>
--- src/layout/SucceedPage/index.js
+++ src/layout/SucceedPage/index.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCart } from "../../store/user/addToCartSlice";
 
-function SucceedPage(props) {
-    return (
+function SucceedPage() {
+  const dispatch = useDispatch();
+  const newCartArr = useSelector((state) => state.addToCartReducer.carts);
+
+  const numbers = parseInt(newCartArr.map((item) => item.numberPeople));
+
+  const price = parseInt(newCartArr.map((item) => item.cost));
+
+  console.log("numbers", numbers);
+  console.log("price", price);
+
+  const totalPrice = numbers * price;
+
+  console.log("totalPrice", totalPrice);
+
+  useEffect(() => {
+    dispatch(fetchCart());
+  }, [dispatch]);
+
+  return (
+    <div>
+      {newCartArr?.map((item) => (
         <div>
-            
+          <p>{item.idTour}</p>
+          <p>{item.nameTour}</p>
+          <p>{totalPrice}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default SucceedPage;
